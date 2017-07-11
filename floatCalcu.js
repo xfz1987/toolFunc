1e7d6c71e6d044103454c09d4d4104062e38b2b5
gzf <281345774@qq.com>
2017-07-11 15:13:17 +0800

    init

---
+++ floatCalcu.js
@@ -0,0 +1,28 @@
+//浮点数计算
+define([],function(){
+	//加法/减法
+	Number.prototype.add = function(arg){
+		var arg1=this,arg2=arg,r1,r2,m; 
+		try{r1=arg1.toString().split('.')[1].length}catch(e){r1=0} 
+		try{r2=arg2.toString().split('.')[1].length}catch(e){r2=0} 
+		m=Math.pow(10,Math.max(r1,r2));
+		return ((arg1*m+arg2*m)/m);
+	}
+	//乘法
+	Number.prototype.mul = function(arg){
+		var m=0,s1=this.toString(),s2=arg.toString(); 
+		try{m+=s1.split('.')[1].length}catch(e){} 
+		try{m+=s2.split('.')[1].length}catch(e){} 
+		return (Number(s1.replace('.',''))*Number(s2.replace('.',''))/Math.pow(10,m)); 
+	}
+	//除法
+	Number.prototype.div = function(arg){
+		var arg1=this,arg2=arg,t1=0,t2=0,r1,r2; 
+		try{t1=arg1.toString().split('.')[1].length}catch(e){} 
+		try{t2=arg2.toString().split('.')[1].length}catch(e){} 
+		r1=Number(arg1.toString().replace('.',''));
+		r2=Number(arg2.toString().replace('.',''));
+		return ((r1/r2)*Math.pow(10,t2-t1));
+	} 
+
+});
